94aec94bd985d21426a13935879af67fdbfbc0dc
Thunder-hunt <rainarrizky06@gmail.com>
2026-09-16 11:42:16 +0700

    Update TextWidget to enhance UI and content for Flutter learning

--- lib/widgets/text_widget.dart
+++ lib/widgets/text_widget.dart
@@ -6,13 +6,52 @@ class TextWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Text Widget')),
-      body: const Center(
-        child: Text(
-          'Halo, Flutter!',
-          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+      appBar: AppBar(
+        title: const Text(
+          'Belajar Text Widget',
+          style: TextStyle(color: Colors.white),
         ),
+        backgroundColor: Colors.purpleAccent,
       ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Belajar Flutter',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Mobile Development',
+                style: TextStyle(fontSize: 18, color: Colors.blue),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                color: Colors.blueAccent,
+                padding: const EdgeInsets.only(top: 10, left: 30, right: 30, bottom: 10),
+                child: const Text(
+                  'SMK Kelas XII SIJA 2',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Flutter mempermudah proses\npembuatan aplikasi mobile.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  height: 1.5,
+                  color: Colors.deepPurple,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
   }
 }
